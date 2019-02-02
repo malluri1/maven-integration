0ed27405c5e6d16953c90575589a546342a43555
lakshmi <prasadlakshmi009@gmail.com>
2019-02-02 22:40:02 +0530

    firstmavenproject1

---
+++ src/main/java/windowsproject2/SimpleService.java
@@ -0,0 +1,15 @@
+package windowsproject2;
+
+import com.google.gwt.angular.client.NgInject;
+
+@NgInject(name="simple")
+public class SimpleService {
+
+	public String greet(String name) {
+		return "Hello " + name;
+	}
+	
+	public String dismiss(String name) {
+		return "Goodbye " + name;
+	}
+}
